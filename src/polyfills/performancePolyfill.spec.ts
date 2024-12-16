69177331c5b99ba459120b3df0ca926f97843b8d
CeriumCerealBowl <159857129+CeriumCerealBowl@users.noreply.github.com>
2024-12-15 22:27:36 -0600

    Delete src directory

--- src/polyfills/performancePolyfill.spec.ts
+++
@@ -1,7 +0,0 @@
-import { monotonicTime } from "./performancePolyfill";
-
-describe("Check performance.now polyfill", () => {
-    it("monotonicTime should return number", () => {
-        expect(typeof monotonicTime()).toEqual("number");
-    });
-});
