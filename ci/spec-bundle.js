9115d7f41edec93ceaf821c2a336de4b883f4074
CeriumCerealBowl <159857129+CeriumCerealBowl@users.noreply.github.com>
2024-12-15 22:26:57 -0600

    Delete ci directory

--- ci/spec-bundle.js
+++
@@ -1,7 +0,0 @@
-var testContext = require.context("../src", true, /\.spec\.ts$/);
-
-function requireAll(context) {
-    return context.keys().map(context);
-}
-
-var modules = requireAll(testContext);
